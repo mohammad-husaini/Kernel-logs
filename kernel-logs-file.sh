f12bc19951282d0ce9f5a1d46380bd13e7766225
mohammad-husaini <mo.r.husini@gmail.com>
2023-08-11 22:28:57 +0300

    V 1.2

--- kernel-logs-file.sh
+++ kernel-logs-file.sh
@@ -1,19 +1,16 @@
-#!/bin/bash
+#!/bin/sh
+
 
 set -e
-
-log_dir="/var/log/kernel-logs"
 log_filename="kernel-logs-$(date +'%Y-%m-%dT%H-%M')"
-log_compressfile="$log_dir/$log_filename.tar.bz2"
-dbxcli=/home/mohammad/bin/dbxcli
+log_compressfile="$log_filename.tar.bz2"
 
 
-journalctl --since "1 hour ago" -k > $log_dir/$log_filename.txt
 
-tar -cjvf $log_compressfile $log_dir/$log_filename.txt
+journalctl --since "1 hour ago" -k > $log_filename.txt
 
-rm -f $log_dir/$log_filename.txt
+tar -cjvf $log_compressfile $log_filename.txt
 
-$dbxcli put "$log_compressfile" /kernal_logs/$log_filename
+dbxcli put "$log_compressfile" /kernal_logs/$log_filename
 
-rm -f $log_compressfile
+rm -f $log_compressfile $log_filename.txt
